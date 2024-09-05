f15a96f9634b8c1749c55bd4e57f173ac09bb497
INAUTX <151594185+INAUTX@users.noreply.github.com>
2024-09-05 15:44:14 -0500

    Create project base

---
+++ src/main/java/com/riwi/sitekeeper/SitekeeperApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.sitekeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitekeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitekeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riwi/sitekeeper/SitekeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.sitekeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitekeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
