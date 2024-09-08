b8c589e003310de06f819c27e8c819e047c860f4
santiagozuluaga23 <santiago.zuluagad2000@gmail.com>
2024-09-08 17:22:17 -0500

    Create Object entity

---
+++ src/main/java/com/riwi/sitekeeper/entitites/ObjectEntity.java
@@ -0,0 +1,33 @@
+package com.riwi.sitekeeper.entitites;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "objects")
+public class ObjectEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(name = "name", nullable = false)
+    String name;
+
+    @Column(name = "description", nullable = false)
+    String description;
+
+    @Column(name = "image", nullable = false)
+    String image;
+
+    @Column(name = "space_id", nullable = false)
+    Long spaceId;
+
+}
