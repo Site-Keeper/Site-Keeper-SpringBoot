5e0abb798251202be5eedcbafacb6a9affb80b9e
Mahi_G13 <maicolgallego14@gmail.com>
2024-09-11 17:10:18 -0500

    Added swagger documentation for each endpoint

--- src/main/java/com/riwi/sitekeeper/controllers/impl/SpaceController.java
+++ src/main/java/com/riwi/sitekeeper/controllers/impl/SpaceController.java
@@ -2,6 +2,8 @@ package com.riwi.sitekeeper.controllers.impl;
 
 import com.riwi.sitekeeper.entitites.SpaceEntity;
 import com.riwi.sitekeeper.services.SpaceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
@@ -18,21 +20,31 @@ public class SpaceController {
     SpaceService spaceService;
 
     @PostMapping
+    @Operation(summary = "Create a new space", description = "Creates a new Space")
+    @ApiResponse(responseCode = "201", description = "Space successfully created")
     public ResponseEntity<SpaceEntity> CreateSpace (@RequestBody SpaceEntity space){
         return ResponseEntity.ok(spaceService.create(space));
     }
 
     @PutMapping
+    @Operation(summary = "Update an Space", description = "Updates an space")
+    @ApiResponse(responseCode = "200", description = "Space successfully Updated")
+    @ApiResponse(responseCode = "404", description = "Unable to update Space")
     public ResponseEntity<SpaceEntity> UpdateSpace (@RequestBody SpaceEntity space){
         return ResponseEntity.ok(spaceService.update(space));
     }
 
     @GetMapping
+    @Operation(summary = "List all spaces", description = "Lists all Spaces")
+    @ApiResponse(responseCode = "200", description = "Spaces successfully gathered")
     public ResponseEntity<List<SpaceEntity>> getAllSpaces(){
         return ResponseEntity.ok(spaceService.getAll());
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Find Space by Id", description = "Gets an Space based on the Id")
+    @ApiResponse(responseCode = "200", description = "Space successfully found")
+    @ApiResponse(responseCode = "404", description = "Unable to find Space with the Id provided")
     public ResponseEntity<SpaceEntity> getSpaceById(@PathVariable Long id){
         Optional<SpaceEntity> space = spaceService.getById(id);
         return space.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
